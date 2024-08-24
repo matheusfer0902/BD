0b56ed4f58e125b4dbb84a94eaed21f0de487e62
BiaMouraa <beatriz.moura3@academico.ufpb.br>
2024-08-24 20:22:15 -0300

    add Pesquisa Nome e ID

--- parte 1/module.py
+++ parte 1/module.py
@@ -34,7 +34,20 @@ class Book:
                 f"Qtdd: {self.qntdd}\n")
         return
 
-    def updatePrice(self, bookID, newPrice=None):
+    def updatePrice(self):
+
+        print("Selecione o id do livro que você deseja alterar\n")
+
+        op = int(input())
+
+        print("Qual é o novo preço? ")
+        newPrice = float(input())
+
+        comando = f'UPDATE estoque SET WHERE price = {newPrice} WHERE idlivro = {op}'
+
+        connection.cursor.execute(comando)
+
+        connection.connection.commit()
         return
 
     def removeBook(self, bookID):
