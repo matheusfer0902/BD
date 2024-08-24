8de8b72af3ead3a12e296173def041c7cfdb97f0
Matheus Ferreira <matheusfer0902@gmail.com>
2024-08-24 19:19:13 -0300

    connection arq

--- parte 1/connection.py
+++ parte 1/connection.py
@@ -3,7 +3,7 @@ import mysql.connector
 connection = mysql.connector.connect(
     host='127.0.0.1',
     user='root',
-    password='738291',
+    password='pingo0902',
     database='livraria',
 )
 
