26817ac3618e18e29b0077a29e15ab679c6219aa
Matheus Ferreira <matheusfer0902@gmail.com>
2024-08-25 20:04:55 -0300

    Add opçãp de alterar o preço em um livro existente quando for adicionado

--- parte 1/module.py
+++ parte 1/module.py
@@ -1,4 +1,5 @@
 import connection
+import os
 
 def commitDB(comando):
     connection.cursor.execute(comando)
@@ -23,6 +24,14 @@ class Book:
                 f"Publisher: {self.publisher}\n"
                 f"Price: {self.price}\n"
                 f"Qtdd: {self.qntdd}\n")
+        
+    def getFloatInputForClass(self, prompt):
+        while True:
+            try:
+                value = float(input(prompt))
+                return value
+            except ValueError:
+                print("Entrada inválida. Por favor, digite um número válido.")
 
     def insertBook(self):
         comando = f'SELECT * FROM estoque WHERE name = "{self.name}" AND author = "{self.author}" AND publisher = "{self.publisher}"'
@@ -34,10 +43,31 @@ class Book:
             print("Inserção feita com sucesso!!\n")
 
         else:
-            id = resultado[0][0]
-            qntddID = resultado[0][5]
-            self.updateQuantity(id, self.qntdd+qntddID)
-            print("Livro já estava inserido. Quantidade atualizada!")
+            os.system('cls')
+            priceInBD = float(resultado[0][4])
+            if priceInBD != self.price:
+                print("O livro já está inserido e a quantidade será atualizada\n")
+                priceOp = self.getFloatInputForClass("Porem o preço em estoque está diferente. Escolha como prosseguir\n"
+                    "1 - Continuar com o preço em estoque.\n"
+                    "2 - atualizar estoque com o novo preço.\n"
+                )
+                match priceOp:
+                    case 1:
+                        id = resultado[0][0]
+                        qntddID = resultado[0][5]
+                        self.updateQuantity(id, self.qntdd+qntddID)
+                        print("Quantidade atualizada!")
+                    case 2:
+                        id = resultado[0][0]
+                        qntddID = resultado[0][5]
+                        self.updatePrice(id, self.price)
+                        self.updateQuantity(id, self.qntdd+qntddID)
+                        print("Quantidade e preço atualizados!")
+            else:
+                id = resultado[0][0]
+                qntddID = resultado[0][5]
+                self.updateQuantity(id, self.qntdd+qntddID)
+                print("Quantidade atualizada!")
         return
 
     def showData(self, id):
