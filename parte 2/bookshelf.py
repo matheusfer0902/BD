9bbba8fb41f165abdcf4671a74e9f11d0eefd40c
BiaMouraa <beatriz.moura3@academico.ufpb.br>
2024-10-09 11:23:59 -0300

    add CRUD estoque(parte1)

---
+++ parte 2/bookshelf.py
@@ -0,0 +1,139 @@
+import connection
+import os
+
+class Book:
+    def __init__(self, name, author, publisher, price, qntdd):
+        self.name = name
+        self.author = author
+        self.publisher = publisher
+        self.price = price
+        self.qntdd = qntdd
+        
+    def getFloatInputForClass(self, prompt):
+        while True:
+            try:
+                value = float(input(prompt))
+                return value
+            except ValueError:
+                print("Entrada inválida. Por favor, digite um número válido.")
+    
+    def printBook(self, info):
+        print("Nome: ", info[0])
+        print("Autor: ", info[1])
+        print("Editora: ", info[2])
+        print("Preço: ", info[3])
+        print("Qntdd ", info[4], "\n")
+
+    def insertBook(self, db, info):
+        comando = """SELECT * FROM estoque WHERE name = %s AND author = %s AND publisher = %s"""
+        search = (info[0], info[1], info[2])
+        resultado = db.readDB(comando, search)
+        for row in resultado:
+            print("Id = ", row[0], )
+            print("Name = ", row[1], )
+            print("Author = ", row[2], )
+            print("Publisher = ", row[3], )
+            print("Price = ", row[4], )
+            print("Qntdd = ", row[5], "\n")
+
+        if resultado == []:
+            comando = """ INSERT INTO estoque(name, author, publisher, price, quantidade) VALUES (%s,%s,%s,%s,%s)"""
+            db.commitDB(comando, info)
+            print("Inserção feita com sucesso!!\n")
+        else:
+            os.system('cls')
+            priceInBD = float(resultado[0][4])
+            if priceInBD != info[3]:
+                print("O livro já está inserido e a quantidade será atualizada\n"
+                        "Porem o preço em estoque está diferente. Escolha como prosseguir\n")
+                while True:
+                    priceOp = self.getFloatInputForClass("1 - Continuar com o preço em estoque.\n"
+                                                        "2 - atualizar estoque com o novo preço.\n")
+                    match priceOp:
+                        case 1:
+                            id = resultado[0][0]
+                            qntddID = resultado[0][5]
+                            self.updateQuantity(id, info[4]+qntddID)
+                            print("Quantidade atualizada!")
+                            break
+                        case 2:
+                            id = resultado[0][0]
+                            qntddID = resultado[0][5]
+                            self.updatePrice(id, info[3])
+                            self.updateQuantity(id, info[4]+qntddID)
+                            print("Quantidade e preço atualizados!")
+                            break
+                        case _:
+                            print("Opção Inválida, tente novamente.\n")
+            else:
+                id = resultado[0][0]
+                qntddID = resultado[0][5]
+                self.updateQuantity(id, info[4]+qntddID)
+                print("Quantidade atualizada!")
+        return
+
+    def showData(self, id):
+        print(f"\n(ID): {id}\n"
+                f"Name: {self.name}\n"
+                f"Author: {self.author}\n"
+                f"Publisher: {self.publisher}\n"
+                f"Price: {self.price}\n"
+                f"Qtdd: {self.qntdd}\n")
+        return
+
+    def updatePrice(self, db, id, newPrice):
+
+        # comando = f'UPDATE estoque SET price = {newPrice} WHERE id_book = {id}'
+        comando = "UPDATE estoque SET price = %s WHERE id_book = %s"
+        db.commitDB(comando,(newPrice, id))
+        return
+    
+    def updateName(self, db, id, newName):
+        comando = "UPDATE estoque SET name = %s WHERE id_book = %s"
+        db.commitDB(comando,(newName, id))
+        return
+        
+    def updateAuthor(self, db, id, newAuthor):
+        comando = "UPDATE estoque SET author = %s WHERE id_book = %s"
+        db.commitDB(comando,(newAuthor, id))
+        return
+
+    def updatePublisher(self, db, id, newPublisher):
+        comando = "UPDATE estoque SET publisher = %s WHERE id_book = %s"
+        db.commitDB(comando,(newPublisher, id))
+        return
+
+    def updateQuantity(self, db, id, newQuant):
+        comando = "UPDATE estoque SET quantidade = %s WHERE id_book = %s"
+        db.commitDB(comando,(newQuant, id))
+        return
+
+
+    def removeBook(self, db, qntdd, quantidade, id):
+        
+        
+        if(qntdd - quantidade <= 0):
+            comando = "DELETE FROM estoque WHERE id_book = %s"
+            db.commitDB(comando, (id,))
+
+        else:
+            comando = "UPDATE estoque SET quantidade = %s WHERE id_book = %s"
+            info = ((qntdd-quantidade), id)
+            db.commitDB(comando, info)
+        print("Remoção feita com sucesso!!")
+
+
+        return
+    
+    def searchByName(self, db, name):
+        comando = "SELECT * from estoque WHERE name LIKE %s"
+        resultado = db.readDB(comando, (f"%{name}%",))
+        if resultado == []:
+            print("Nome não encontrado!\n")
+        else:
+            return resultado
+    
+    def searchByID(self, db, id):
+        comando = f'SELECT * FROM estoque WHERE id_book = "{id}"'
+        resultado = db.readDB(comando)
+        return resultado
--- parte 2/main.py
+++ parte 2/main.py
@@ -1,5 +1,36 @@
 import connection
 import users
+import bookshelf
+import os
+
+def mainMenu() :
+    while 1:
+        op = getIntInput("Selecione sua opção\n"
+            "1 - Inserir novo livro\n"
+            "2 - Editar dados\n"
+            "3 - Pesquisar livro\n"
+            "4 - Listar livros\n"
+            "5 - Remover livro\n"
+            "6 - Cadastrar usuário\n"
+            "7 - Sair\n")
+        return op
+
+def searchMenu():
+    while 1:
+        op = getIntInput("Selecione a opção que você deseja pesquisar:\n"
+            "1 - Pesquisar por nome\n"
+            "2 - Pesquisar por ID\n"
+            "3 - Voltar\n")
+        return op
+
+def callShowData(resultado):
+    for row in resultado:
+            print("Id: ", row[0], )
+            print("Nome: ", row[1])
+            print("Autor: ", row[2])
+            print("Editora: ", row[3])
+            print("Preço: ", row[4])
+            print("Qntdd: ", row[5], "\n")
 
 def getStringInput(prompt):
     while True:
@@ -19,14 +50,6 @@ def getFloatInput(prompt):
             except ValueError:
                 print("Entrada inválida. Por favor, digite um número válido.")
 
-def getStringToName(prompt):
-    while True:
-            value = input(prompt)
-            if len(value) > 45:
-                print("Entrada inválida. O texto deve ter no máximo 45 caracteres..")
-            else:
-                return value
-
 def getIntInput(prompt):
         while True:
             try:
@@ -34,6 +57,14 @@ def getIntInput(prompt):
                 return value
             except ValueError:
                 print("Entrada inválida. Por favor, digite um número inteiro válido.")
+
+def getStringToName(prompt):
+    while True:
+            value = input(prompt)
+            if len(value) > 45:
+                print("Entrada inválida. O texto deve ter no máximo 45 caracteres..")
+            else:
+                return value
 
 def getBolleanInput(prompt):
         while True:
@@ -47,6 +78,22 @@ def getBolleanInput(prompt):
                     return False
                 case _:
                     print("Opção Inválida\n")
+
+def showAll():
+    comando = "SELECT * FROM estoque"
+    resultado = db.readDB(comando,[])
+    callShowData(resultado)
+
+def updateMenu():
+     while 1:
+        op = getIntInput("O que você deseja alterar?\n"
+            "1 - Titulo\n"
+            "2 - Autor\n"
+            "3 - Editora\n"
+            "4 - Preço\n"
+            "5 - Quantidade\n"
+            "6 - Voltar\n")
+        return op
 
 def registerMenu():
     op = getIntInput("Selecione o tipo de usuário:\n"
@@ -72,6 +119,18 @@ def registerUser():
             case _:
                 print("Opção Inválida\n")
 
+def getBookFromID():
+       
+    id = getIntInput("Selecione o ID: ")
+    comando = "SELECT * from estoque WHERE id_book = %s"
+    resultado = db.readDB(comando, (id,))
+    if resultado == []:
+        print("ID não encontrado!\n")
+        return resultado
+    else:
+        return resultado
+
+
 db = connection.Database(host="26.63.103.162", 
                         database="livraria", 
                         user="postgres", 
@@ -79,6 +138,133 @@ db = connection.Database(host="26.63.103.162",
 db.connect()
 
 
-registerUser()
+on = True
+bookshelf = bookshelf.Book(0,0,0,0,0) #generic object for calling methods
+while(on):
+    os.system('cls')
+    op = int(mainMenu())
+
+    match op:
+        case 1: #CREATE/INSERIR
+            os.system('cls')
+
+            # book = createBook()
+            info = (getStringToName("Digite o nome do livro: "), 
+                    getStringInput("Digite o nome do autor: "),
+                    getStringInput("Digite o nome da editora: "),
+                    getFloatInput("Digite o preço do livro: "),
+                    getIntInput("Digite a quantidade de livros: "))
+            bookshelf.printBook(info)
+            bookshelf.insertBook(db, info)
+
+            input("\nAperte ENTER para continuar...")
+        case 2: #UPDATE/ALTERAR
+            os.system('cls')
+
+            showAll()
+
+            resultado = getBookFromID()
+            id = resultado[0][0]
+
+            upOption = updateMenu()
+            match upOption:
+                case 1: #UPDATE NAME
+                    newName = getStringToName("Qual o novo titulo? ")
+                    bookshelf.updateName(db, id, newName)
+                    print("Titulo Atualizado.")
+                    input("\nAperte ENTER para continuar...")
+                case 2: #UPDATE AUTHOR
+                    newAuthor = getStringInput("Qual o novo Autor? ")
+                    bookshelf.updateAuthor(db, id, newAuthor)
+                    print("Autor Atualizado.")
+                    input("\nAperte ENTER para continuar...")
+                case 3: #UPDATE PUBLISHER
+                    newPublisher = getStringInput("Qual a nova editora? ")
+                    bookshelf.updatePublisher(db, id, newPublisher)
+                    print("editora Atualizada.")
+                    input("\nAperte ENTER para continuar...")
+                case 4: #UPDATE PRICE
+                    newPrice = getFloatInput("Qual o novo preço? ")
+                    bookshelf.updatePrice(db, id, newPrice)
+                    print("Preço Atualizado.")
+                    input("\nAperte ENTER para continuar...")
+                case 5: #UPDATE QUANTITY
+                    newQuant = getIntInput("Qual a nova quantidade? ")
+                    bookshelf.updateQuantity(db, id, newQuant)
+                    print("quantidade Atualizada.")
+                    input("\nAperte ENTER para continuar...")
+                case 6: #BACK TO MENU
+                    input("\nAperte ENTER para voltar ao menu...")
+                case _:
+                    print("Opção inválida")
+        case 3: #SEARCH/PESQUISAR
+            os.system('cls')
+            idOrName = int(searchMenu())
+            match idOrName:
+                case 1: #NAME
+                    os.system('cls')
+                    searchName = getStringInput("Digite o nome para pesquisa: ")
+                    resultado = bookshelf.searchByName(db, searchName)
+                    if resultado:
+                        callShowData(resultado)
+
+                    input("\nAperte ENTER para continuar...")
+                case 2: #ID
+                    os.system('cls')
+
+                    resultado = getBookFromID()
+                    if resultado:
+                        callShowData(resultado)
+                    # bookshelf.showData(resultado[0])
+
+                    input("\nAperte ENTER para continuar...")
+                case 3: #BACK TO MENU
+                    input("\nAperte ENTER para voltar ao menu...")
+                case _:
+                    print("Opção inválida")   
+        case 4: #SHOW ALL/LISTAR TODOS
+            os.system('cls')
+
+            showAll()
+            
+            input("\nAperte ENTER para continuar...")
+        case 5: #REMOVE/REMOVER
+            showAll()
+            
+            resultado = getBookFromID()
+            callShowData(resultado)
+
+            id = resultado[0][0]
+            qntdd = int(resultado[0][5])
+
+            quantidade = getIntInput("Qual a quantidade a ser removida? ")
+            if(qntdd - quantidade <= 0):
+                print("ATENÇÃO: Essa ação irá EXCLUIR o livro do estoque\n"
+                        "Essa ação não poderá ser desfeita!\n")
+                while True:
+                    op = getIntInput("Deseja continuar?[0 - não ou 1 - sim] ")
+                    if op == 1:
+                        bookshelf.removeBook(db, qntdd, quantidade, id)
+                        break
+                    elif op == 0:
+                        break
+                    elif op != 0 & op != 1:
+                        print("Opção Inválida, tente novamente.")
+
+            else:
+                bookshelf.removeBook(db, qntdd, quantidade, id)
+
+            input("\nAperte ENTER para continuar...")
+        case 6:
+            os.system('cls')
+            registerUser()
+        case 7: #EXIT/SAIR
+            on = False
+        case _:
+            os.system('cls')
+            print("Opção inválida")
+        
+    
+print("Programa Encerrado!\n")
 
 db.close()
